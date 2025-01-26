a548e2cd1e1408ce079a8c039b25a5bb74f401df
KemalQ <292126@ogr.ktu.edu.tr>
2025-01-26 17:45:44 +0300

    added logging, implemented api to send message from bot

--- src/main/java/com/staj/staj/controller/TelegramBot.java
+++ src/main/java/com/staj/staj/controller/TelegramBot.java
@@ -1,14 +1,17 @@
 package com.staj.staj.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.meta.generics.BotOptions;
-import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 @Component
+@Slf4j
 public class TelegramBot extends TelegramLongPollingBot {
 
     @Value("${telegram.bot.username}")
@@ -28,8 +31,21 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        var originalMessage = update.getMessage();
-        System.out.println(originalMessage.getText());
-    }
+        Message originalMessage = update.getMessage();
+        log.info("recieved message: {}", originalMessage.getText());
 
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
+    }
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try{
+                execute(message);
+            } catch (TelegramApiException e){
+                log.error(String.valueOf(e));
+            }
+        }
+    }
 }
