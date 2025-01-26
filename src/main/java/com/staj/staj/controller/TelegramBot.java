39920c0025af29403130b7db746c95091afe665a
KemalQ <292126@ogr.ktu.edu.tr>
2025-01-26 15:46:16 +0300

    @Value added

--- src/main/java/com/staj/staj/controller/TelegramBot.java
+++ src/main/java/com/staj/staj/controller/TelegramBot.java
@@ -1,20 +1,31 @@
 package com.staj.staj.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Component
 public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.username}")
+    private String userName;
+
+    @Value("${telegram.bot.token}")
+    private String tokenName;
+
     @Override
     public String getBotUsername() {
-        return null;
+        return userName;
     }
     @Override
     public String getBotToken(){
-        return null;
+        return tokenName;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        var originalMessage = update.getMessage();
+        System.out.println(originalMessage.getText());
     }
 }
