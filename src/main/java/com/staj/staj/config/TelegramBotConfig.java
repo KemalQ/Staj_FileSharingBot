2e5962122222253589bbcf4f94b4e664f9df6043
KemalQ <292126@ogr.ktu.edu.tr>
2025-01-26 16:53:59 +0300

    Java version downgraded to 11, added config for telegram bot

---
+++ src/main/java/com/staj/staj/config/TelegramBotConfig.java
@@ -0,0 +1,18 @@
+package com.staj.staj.config;
+
+import com.staj.staj.controller.TelegramBot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class TelegramBotConfig {
+    @Bean
+    public TelegramBotsApi telegramBotsApi(TelegramBot bot) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
+        return telegramBotsApi;
+    }
+}
