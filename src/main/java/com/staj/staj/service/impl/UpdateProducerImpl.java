15aa5edea8c665854ba9a547ec5d1e9daf5e5153
KemalQ <292126@ogr.ktu.edu.tr>
2025-01-30 14:35:47 +0300

    RabbitConfiguration added, configured the transmission of messages from the service

--- src/main/java/com/staj/staj/service/impl/UpdateProducerImpl.java
+++ src/main/java/com/staj/staj/service/impl/UpdateProducerImpl.java
@@ -3,15 +3,21 @@ package com.staj.staj.service.impl;
 import com.staj.staj.service.UpdateProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.jvnet.hk2.annotations.Service;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
 @Slf4j
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
-
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
